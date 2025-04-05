808bde12d41b6f3844be6871da7e2ead4e6792cc
alok birge <167791634+alokbirge@users.noreply.github.com>
2025-04-05 20:30:36 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,12 @@
+<html >
+    <body style="background-color:burlywood;"> 
+    <center> 
+   
+    welcome <b> <?php echo $_GET["name"]; ?> <br> </b>
+    your password is <b> <?php echo $_GET["password"];?> <br> </b>
+    your date of brith <b> <?php echo $_GET["sub"];?> <br> </b>
+ 
+      
+    </center> 
+    </body>
+ </html> 
